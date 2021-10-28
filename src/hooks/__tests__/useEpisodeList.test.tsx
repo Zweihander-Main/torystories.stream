21a6aabb2748054611fd7e63b97d39c0b958664f
Zweihänder <zweidev@zweihander.me>
2021-10-28 00:09:17 -0400

    test(useEpisodeList): render and return data

---
+++ src/hooks/__tests__/useEpisodeList.test.tsx
@@ -0,0 +1,36 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useEpisodeList from 'hooks/useEpisodeList';
+import { useStaticQuery } from 'gatsby';
+
+describe('useEpisodeList hook', () => {
+	beforeAll(() => {
+		(useStaticQuery as jest.Mock).mockReturnValue({
+			allMarkdownRemark: {
+				edges: [
+					{
+						node: {
+							id: '1',
+							frontmatter: {
+								audioFile: {
+									publicURL: '/ep1',
+								},
+							},
+						},
+					},
+					{
+						node: {
+							id: '2',
+						},
+					},
+				],
+			},
+		});
+	});
+
+	it('should render and return valid data', () => {
+		const { result } = renderHook(() => useEpisodeList());
+		const { episodeArray, getEpByID } = result.current;
+		expect(episodeArray).toHaveLength(2);
+		expect(getEpByID('1').audioURL).toBe('/ep1');
+	});
+});
