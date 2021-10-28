e063c48bfc646c80f5677a6367711aed383490c7
Zweihänder <zweidev@zweihander.me>
2021-10-28 19:40:04 -0400

    fix(useepisodelist): memoize static values

--- src/hooks/useEpisodeList.tsx
+++ src/hooks/useEpisodeList.tsx
@@ -1,5 +1,6 @@
 import { useStaticQuery, graphql } from 'gatsby';
 import { IGatsbyImageData } from 'gatsby-plugin-image';
+import { useCallback, useMemo } from 'react';
 
 interface episodeData {
 	title: string;
@@ -7,7 +8,7 @@ interface episodeData {
 	date: string;
 	episodeNum: number;
 	featuredImage: IGatsbyImageData | null;
-	audioURL: string | null;
+	audioURL: string | undefined;
 	slug: string;
 }
 
@@ -58,38 +59,46 @@ const useEpisodeList = (): EpisodeListProps => {
 		`
 	);
 
-	const episodeLookupTable: { [key: string]: episodeData } = {};
-	episodeListData.allMarkdownRemark.edges.forEach(({ node: episode }) => {
-		const { id, excerpt, frontmatter, fields } = episode;
-		let featuredImage = null;
-		if (frontmatter?.featuredImage) {
-			featuredImage =
-				frontmatter?.featuredImage.childImageSharp?.gatsbyImageData ||
-				null;
-		}
-		episodeLookupTable[id] = {
-			title: frontmatter?.title || '',
-			blurb: frontmatter?.description || excerpt || '',
-			date: frontmatter?.date || '',
-			episodeNum: frontmatter?.episodeNum || 0,
-			featuredImage,
-			audioURL: frontmatter?.audioFile?.publicURL || null,
-			slug: fields?.slug || '',
-		};
-	});
-
-	const getEpByID = (id: string) => {
-		return episodeLookupTable[id];
-	};
-
-	const episodeArray = Object.entries(episodeLookupTable)
-		.map(([id, epData]) => {
-			return { id, ...epData };
-		})
-		.sort((a, b) => {
-			// most recent numbers first
-			return b.episodeNum - a.episodeNum;
+	const memoizedEpisodeLookupTable = useMemo(() => {
+		const episodeLookupTable: { [key: string]: episodeData } = {};
+		episodeListData.allMarkdownRemark.edges.forEach(({ node: episode }) => {
+			const { id, excerpt, frontmatter, fields } = episode;
+			let featuredImage = null;
+			if (frontmatter?.featuredImage) {
+				featuredImage =
+					frontmatter?.featuredImage.childImageSharp
+						?.gatsbyImageData || null;
+			}
+			episodeLookupTable[id] = {
+				title: frontmatter?.title || '',
+				blurb: frontmatter?.description || excerpt || '',
+				date: frontmatter?.date || '',
+				episodeNum: frontmatter?.episodeNum || 0,
+				featuredImage,
+				audioURL: frontmatter?.audioFile?.publicURL || undefined,
+				slug: fields?.slug || '',
+			};
 		});
+		return episodeLookupTable;
+	}, [episodeListData]);
+
+	const getEpByID = useCallback(
+		(id: string) => {
+			return memoizedEpisodeLookupTable[id];
+		},
+		[memoizedEpisodeLookupTable]
+	);
+
+	const episodeArray = useMemo(() => {
+		return Object.entries(memoizedEpisodeLookupTable)
+			.map(([id, epData]) => {
+				return { id, ...epData };
+			})
+			.sort((a, b) => {
+				// most recent numbers first
+				return b.episodeNum - a.episodeNum;
+			});
+	}, [memoizedEpisodeLookupTable]);
 
 	return {
 		getEpByID,
