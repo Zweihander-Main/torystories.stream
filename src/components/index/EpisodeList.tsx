3f5af6c26f3578cf2d7569e6c776ab8e12fc0877
Zweihänder <zweidev@zweihander.me>
2021-10-29 16:57:23 -0400

    perf(episodelist): stop handler rerendering

--- src/components/index/EpisodeList.tsx
+++ src/components/index/EpisodeList.tsx
@@ -1,10 +1,56 @@
-import React, { useContext } from 'react';
+import React, { memo, useContext } from 'react';
 import useEpisodeList from 'hooks/useEpisodeList';
-import { GatsbyImage } from 'gatsby-plugin-image';
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image';
 import { Link } from 'gatsby';
 import { RiPlayCircleLine } from 'react-icons/ri';
 import TrackContext from 'contexts/TrackContext';
 import PlayerStateContext from 'contexts/PlayerStateContext';
+
+type EpisodeImageProps = {
+	featuredImage: IGatsbyImageData | null;
+	title: string;
+	shouldDisplayIcon: boolean;
+	handlePlayClick: (e: React.MouseEvent<SVGElement>, id: string) => void;
+	id: string;
+};
+
+const EpisodeImage: React.FC<EpisodeImageProps> = ({
+	featuredImage,
+	title,
+	shouldDisplayIcon,
+	handlePlayClick,
+	id,
+}) => {
+	if (featuredImage) {
+		return (
+			<div className="grid grid-rows-none grid-cols-none">
+				{shouldDisplayIcon && (
+					<RiPlayCircleLine
+						size="100%"
+						className="h-full self-center justify-self-center row-start-1 row-end-1 col-start-1 col-end-1 z-20 text-white opacity-20 hover:opacity-80 cursor-pointer"
+						onClick={(e) => handlePlayClick(e, id)}
+					/>
+				)}
+				<GatsbyImage
+					image={featuredImage}
+					alt={title}
+					objectFit={'cover'}
+					className="h-full self-center justify-self-center row-start-1 row-end-1 col-start-1 col-end-1 z-10"
+				/>
+			</div>
+		);
+	}
+	return null;
+};
+
+const MemoizedCoverImage = memo(
+	EpisodeImage,
+	(prevProps, nextProps) =>
+		prevProps.featuredImage === nextProps.featuredImage &&
+		prevProps.title === nextProps.title &&
+		prevProps.shouldDisplayIcon === nextProps.shouldDisplayIcon &&
+		prevProps.id === nextProps.id
+);
 
 const EpisodeList: React.FC = () => {
 	const { episodeArray } = useEpisodeList();
@@ -43,25 +89,15 @@ const EpisodeList: React.FC = () => {
 										{blurb}
 									</p>
 								</div>
-								<div className="grid grid-rows-none grid-cols-none">
-									{(!isPlayerPlaying || id !== trackId) && (
-										<RiPlayCircleLine
-											size="100%"
-											className="h-full self-center justify-self-center row-start-1 row-end-1 col-start-1 col-end-1 z-20 text-white opacity-20 hover:opacity-80 cursor-pointer"
-											onClick={(e) =>
-												handlePlayClick(e, id)
-											}
-										/>
-									)}
-									{featuredImage && (
-										<GatsbyImage
-											image={featuredImage}
-											alt={title}
-											objectFit={'cover'}
-											className="h-full self-center justify-self-center row-start-1 row-end-1 col-start-1 col-end-1 z-10"
-										/>
-									)}
-								</div>
+								<MemoizedCoverImage
+									featuredImage={featuredImage}
+									title={title}
+									shouldDisplayIcon={
+										!isPlayerPlaying || id !== trackId
+									}
+									handlePlayClick={handlePlayClick}
+									id={id}
+								/>
 							</div>
 						</Link>
 					</div>
