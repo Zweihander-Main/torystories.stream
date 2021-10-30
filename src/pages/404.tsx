d07a7459df0eb7241bc12c7454fdbf4a357068ad
Zweihänder <zweidev@zweihander.me>
2021-10-29 20:41:44 -0400

    fix(404): add in link to home

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Layout from 'components/structure/Layout';
 import SEO from 'components/structure/SEO';
+import { Link } from 'gatsby';
 
 const NotFound: React.FC = () => {
 	return (
 		<Layout>
 			<SEO title="404: Not Found" />
-			Page Not Found
+			<p className="mt-32">Sorry, not found!</p>
+			<Link to="/">Go to home page</Link>
 		</Layout>
 	);
 };
