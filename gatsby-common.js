0fbd7a60bf892bc01ea5072dbceb6e365733feee
Zweihänder <zweidev@zweihander.me>
2021-11-11 16:05:44 -0500

    fix(layout): remove noscript message

--- gatsby-common.js
+++ gatsby-common.js
@@ -4,15 +4,4 @@ import AppProvider from './src/contexts/AppContext';
 
 export const wrapRoot = ({ element }) => <AppProvider>{element}</AppProvider>;
 
-export const wrapPage = ({ element }) => (
-	<>
-		<noscript className="static top-0 w-full text-center text-white bg-deepPurple">
-			This site will work without JavaScript but interactive elements like
-			the podcast player will not.{' '}
-			<a href="https://github.com/Zweihander-Main/torystories.stream">
-				Source code available.
-			</a>
-		</noscript>
-		<Player>{element}</Player>
-	</>
-);
+export const wrapPage = ({ element }) => <Player>{element}</Player>;
