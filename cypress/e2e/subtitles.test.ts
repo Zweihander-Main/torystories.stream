09ebc41de97fd5c0d8f79daa7ffe90c86f69f1a4
Zweihänder <zweidev@zweihander.me>
2023-05-11 17:21:40 -0400

    test(subtitles): add in test retries for ci

--- cypress/e2e/subtitles.test.ts
+++ cypress/e2e/subtitles.test.ts
@@ -1,34 +1,40 @@
-describe('Subtitles ', () => {
-	beforeEach(() => {
-		cy.visitAndSpyStorage('/', 'getItem');
-		cy.findByTitle(/Episode Info and Subtitles/).click();
-	});
-	it('change as player is playing', () => {
-		cy.findByLabelText('Seek and progress slider').as('slider');
-		cy.findByLabelText('Start playback').as('playbutton');
-		cy.get('@slider').invoke('val').should('eq', '0');
-		cy.get('@playbutton').click();
-		cy.get('@slider').invoke('val', 0.5).trigger('pointerup');
-		cy.get('@slider', { timeout: 10000 })
-			.invoke('val')
-			.should((val) => {
-				let valNum;
-				if (typeof val === 'string') {
-					valNum = parseFloat(val);
-					expect(valNum).to.be.gte(0.5);
-				} else {
-					valNum = val;
-					expect(valNum).to.be.gte(0.5);
-				}
-			});
-		cy.get(
-			'[aria-label="Subtitle Scrolling Text"] > [aria-selected="true"]'
-		).as('initial-sub');
-		cy.get('@initial-sub', { timeout: 20000 }).should(
-			'have.attr',
-			'aria-selected',
-			'false'
-		);
-	});
-});
+describe(
+	'Subtitles ',
+	{
+		retries: { runMode: 4, openMode: 1 },
+	},
+	() => {
+		beforeEach(() => {
+			cy.visitAndSpyStorage('/', 'getItem');
+			cy.findByTitle(/Episode Info and Subtitles/).click();
+		});
+		it('change as player is playing', () => {
+			cy.findByLabelText('Seek and progress slider').as('slider');
+			cy.findByLabelText('Start playback').as('playbutton');
+			cy.get('@slider').invoke('val').should('eq', '0');
+			cy.get('@playbutton').click();
+			cy.get('@slider').invoke('val', 0.5).trigger('pointerup');
+			cy.get('@slider', { timeout: 10000 })
+				.invoke('val')
+				.should((val) => {
+					let valNum;
+					if (typeof val === 'string') {
+						valNum = parseFloat(val);
+						expect(valNum).to.be.gte(0.5);
+					} else {
+						valNum = val;
+						expect(valNum).to.be.gte(0.5);
+					}
+				});
+			cy.get(
+				'[aria-label="Subtitle Scrolling Text"] > [aria-selected="true"]'
+			).as('initial-sub');
+			cy.get('@initial-sub', { timeout: 20000 }).should(
+				'have.attr',
+				'aria-selected',
+				'false'
+			);
+		});
+	}
+);
 export {};
